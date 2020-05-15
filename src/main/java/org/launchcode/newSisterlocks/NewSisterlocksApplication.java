970953ff3ff378823e7bddbad7fadd592e983ab1
qualmless <terasmash@gmail.com>
2020-05-15 12:54:31 -0500

    new project

---
+++ src/main/java/org/launchcode/newSisterlocks/NewSisterlocksApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.newSisterlocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSisterlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSisterlocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/newSisterlocks/NewSisterlocksApplicationTests.java
@@ -0,0 +1,14 @@
+package org.launchcode.newSisterlocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSisterlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
